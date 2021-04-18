48c1c7bd43986b8a9ceeed64710a78f815b4185a
Dhanashri Randive <dhanashrirandive06@gmail.com>
2021-04-18 17:16:38 +0530

    delete class code

--- src/scripts/component/Navbar.jsx
+++ src/scripts/component/Navbar.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Disclosure } from "@headlessui/react";
 import {  MenuIcon, XIcon } from "@heroicons/react/outline";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Navbar() {
   return (
